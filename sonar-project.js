3d4810d20a7dfc9cd9946611fe1c5f98a0f63cc7
Mithun Technologies <devopstrainingblr@gmail.com>
2020-11-06 17:37:38 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -7,7 +7,7 @@ sonarqubeScanner({
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
             'sonar.projectVersion':'1.0',
-	    'sonar.language''js',
+	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
             'sonar.sources': '.',
 	  //'sonar.tests': 'specs',
