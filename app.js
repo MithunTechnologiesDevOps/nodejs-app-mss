211b0b2a67fac2cf9d19643aba10b540940c0f10
Mithun Technologies <devopstrainingbanglore@gmail.com>
2025-01-01 21:21:34 +0530

    Update Code

--- app.js
+++ app.js
@@ -23,11 +23,20 @@ app.get('/mithuntechnologies', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
-    response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
+    response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226  +91-9900012028for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
     response.end();
     
   })
  
+  app.get('/docker', function(request, response) {
+    //response.send("<h2><center>Welcome to Node JS app</h2>");
+    response.write("<h2><center><u>Node JS  Sample App </u></center></h2>");
+	
+    response.write("<h2><center>Welcome to  Docker</h2>" );
+    response.end();
+    
+  })
+
 
 //app.get("/html", function(req,res){
 app.get("/html", function(req,res){
