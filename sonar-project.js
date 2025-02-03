295d66719c0056c04afa851b25ccb7846ce23407
Mithun Technologies <devopstrainingblr@gmail.com>
2025-02-03 07:13:10 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://43.204.38.246:9000/',
+  serverUrl: 'http://18.143.118.149:9980/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_bd17bb860e7c94b743d49515419aea9078492c0e',
+	    'sonar.login': 'squ_0eb0ac3258567e81b3d5638ec75531b7da6d7eee',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
